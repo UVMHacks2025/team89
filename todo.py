dc31d6d03578ec80c8c530f540758786dac8c389
JamesCDavenport <112661163+JamesCDavenport@users.noreply.github.com>
2025-03-01 10:47:22 -0500

    Some progress on todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,24 @@
-print("hello world")
+tasks = {}
+
+def add_task():
+    task_name = input("Task name: ")
+    task_description = input("Task description: ")
+    tasks[task_name] = task_description
+
+def list_tasks():
+    print(tasks)
+
+def finish_task():
+    list_tasks()
+    task_name = input("Which task have you finished? press enter to go back")
+    if task_name:
+        print(f"{tasks[task_name]} --> DONE!")
+        tasks.pop(task_name)
+
+while True:
+    user_choice = input("What to do?\n(1): Add Task\n(2): List Tasks\n")
+    if (user_choice == "1"):
+        add_task()
+    elif (user_choice == "2"):
+        list_tasks()
+    
