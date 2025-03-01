0b9984adfc20508717dde710a816f8f9e4c7218c
Hannah <hdeyst@uvm.edu>
2025-03-01 12:52:35 -0500

    need to print out users as well as their points

--- leaderboard.py
+++ leaderboard.py
@@ -17,11 +17,17 @@ def add_points(user, new_points):
     leaderboard[user] = new_points
 
 def get_top_ten():
+    all = []
     top_ten = []
-    min = 0
-    for user, points in leaderboard:
-        if min < points:
-            min = points
-        while top_ten.size() <= 10 and get_points(user):
-            pass
+    for key in leaderboard:
+        all.append(leaderboard[key])
 
+    while all and len(top_ten) < 10:
+        big = max(all)
+        top_ten.append(big)
+        all.remove(big)
+
+    print(leaderboard[top_ten])
+
+
+get_top_ten()
